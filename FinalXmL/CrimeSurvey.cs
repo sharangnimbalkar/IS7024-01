0b83b4f2e5aac0aaaca8c4ddc7936038729b3f6d
sharangnimbalkar <30493774+sharangnimbalkar@users.noreply.github.com>
2019-12-02 16:53:43 -0500

    Google Maps

---
+++ FinalXmL/CrimeSurvey.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalXmL
+{
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    public partial class CrimeSurvey
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("cityYouLive")]
+        public string CityYouLive { get; set; }
+
+        [JsonProperty("isSafe")]
+        public bool IsSafe { get; set; }
+
+        [JsonProperty("shiftCity")]
+        public string ShiftCity { get; set; }
+    }
+
+    public partial class CrimeSurvey
+    {
+        public static CrimeSurvey[] FromJson(string json) => JsonConvert.DeserializeObject<CrimeSurvey[]>(json, FinalXmL.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this CrimeSurvey[] self) => JsonConvert.SerializeObject(self, FinalXmL.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ FinalXmL/Pages/CrimeSurveys.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalXmL.Pages
+{
+    public class CrimeSurveysModel : PageModel
+    {
+        public CrimeSurvey[] crimeSurveys;
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+
+
+                string crimeSurveyString = webClient.DownloadString("https://smartenrollmentforcrimeresolution.azurewebsites.net/api/CrimeSurveys");
+                crimeSurveys = CrimeSurvey.FromJson(crimeSurveyString);
+                ViewData["crimeSurveys"] = crimeSurveys;
+
+            }
+
+        }
+    }
+}
